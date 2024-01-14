32763aef6819eb4d594798eea8f8e3c52d5c61ad
Natalushka <69918458+Natalushka@users.noreply.github.com>
2024-01-14 21:07:39 +0200

    Update lesson-1.js

--- lesson-1/js/lesson-1.js
+++ lesson-1/js/lesson-1.js
@@ -31,4 +31,61 @@
 // const result = s.replace('#', '');
 // console.log(Number(result));
 
-() => {};
+/*
+  Конкатенація рядків, шаблонні рядки
+ */
+
+// const strA = 'hello';
+// const strB = 'world';
+// const result = strA + " " + strB;
+// console.log(result);
+// const userName = 'Tom';
+// const age = 14;
+// console.log('Hello' + " " + userName + " " + 'your age is' + " " + age);
+// console.log(`Hello ${userName} your age is ${age}`);
+// console.log(`Hello ${userName}`);
+
+// const userName = 'Tom';
+// console.log(userName.length);
+
+// console.log(userName.toUpperCase());
+// console.log(userName.toLowerCase());
+
+/*
+  ? Є рядок 'HTML, CSS, JS, PHP, React, Vue, Node.js, C, C++'.
+  ? Дізнайтеся, чи є в цьому рядку мова: 'JavaScript', або 'JS'?
+  ? Якщо є, дізнайтесь позицію на якій перебуває цей підрядок
+*/
+
+// const str = 'HTML, CSS, JS, PHP, React, Vue, Node.js, C, C++';
+// console.log(str.includes('JavaScript'));
+// console.log(str.includes('JS'));
+// console.log(str.indexOf('JS'));
+// console.log(str[0]);
+// console.log(str.startsWith('H'));
+// console.log(str.endsWith('+'));
+
+// const str = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+// console.log(str);
+// console.log(str.replaceAll('dog', 'cat'));
+
+// let sameDate = '12-01-2024';
+// const result = sameDate.replaceAll('-', '.');
+// console.log(sameDate);
+// console.log(result);
+
+// const str = 'Hello, World';
+// const result = str.slice(-1, 6);
+// console.log(`${result}...`);
+
+// const str = 'Hello, World';
+// const result = str.slice(3);
+// console.log(`${result}...`);
+// console.log(str.split(',').join('-').replace(' ', ''));
+
+// function showUserMessage(userName, price, quantity) {
+// const totalPrice = price * quantity // проміжкова константа
+//    return `Дякуємо, ${userName}! До сплати ${price * quantity} грн.`;
+// }
+// console.log(showUserMessage('Alice', 5000, 200));
+// console.log(showUserMessage('Alex', 1000, 65));
