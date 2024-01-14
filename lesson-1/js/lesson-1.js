2704b12faf4cf20db7427bd6e687aef835a9aae1
JohnRealy <branderok@gmail.com>
2024-01-14 19:03:51 +0100

    Коміт і пуш

--- lesson-1/js/lesson-1.js
+++ lesson-1/js/lesson-1.js
@@ -1,2 +1,34 @@
-const number = 20;
-console.log(number);
+/*
+  Математичні оператори (+, -, *, /, %, **)
+*/
+
+/*
+ ? Дано два числа 10 та 20.
+ ? Проведіть над цими числами математичні операції (+ - / * ).
+*/
+
+// const a = 10;
+// const b = '6';
+// const c = 15;
+// const d = 2.6;
+// const e = '2.552%';
+// const s = '#2.33';
+
+// console.log(Math.min(a, b, c));
+// console.log(Math.max(a, b, c));
+
+// console.log(Math.floor(d));
+// console.log(Math.ceil(d));
+// console.log(Math.round(d));
+// console.log(Number(b));
+// console.log(b);
+
+// const result = e.replace(',', '.');
+// console.log(parseFloat(result).toFixed(1));
+
+// console.log(parseFloat(e));
+
+// const result = s.replace('#', '');
+// console.log(Number(result));
+
+() => {};
