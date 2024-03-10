da6a9c49039ef2558526e3384b74856eb5429203
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2024-03-10 18:56:42 +0200

    added task 3

--- lesson-05/module-08/js/app.js
+++ lesson-05/module-08/js/app.js
@@ -57,3 +57,47 @@ function onClick(e) {
 }
 
 //TODO:=========task-03=======Modal==========
+/**
+  |============================
+  | Модальне вікно:
+  |  1 - Реалізувати відкриття модального вікна по кліку по кнопці "Відкрити модалку".
+  |  2 - Реалізувати закриття модального вікна по кліку на крестик і по "ESC". Не забувай знімати слухача події "keydown" після закриття модального вікна.
+  |  Виконуй завдвння послідовно: 
+  |  - Спочатку знайди елементи у Дом дереві, з якими тобі потрібно працювати. 
+  |  - Створи 3 функції onOpenModalBtnElClick, closeModalWindow, onEscClick. Не забувай робити перевірку на цільовий елемент. 
+  |  - Відображення модельного вікна реалізуй заа допомогою бібліотеки basicLightbox і його метода "basicLightbox.create".
+  |============================
+  
+*/
+
+/**
+  |============Розмітка для модального вікна================
+  <div class="modal">
+  <button type="button" class="close-btn js-modal-close">
+    X
+  </button>
+
+  <form class="login-form js-modal__form">
+    <label>
+      Name
+      <input type="text" name="name" placeholder="enter your name" />
+    </label>
+    <label>
+      Email
+      <input type="email" name="email" placeholder="enter your email" />
+    </label>
+    <label>
+      Password
+      <input
+        type="password"
+        name="password"
+        placeholder="enter your password"
+      />
+    </label>
+    <button class="login-btn" type="submit">
+      Login
+    </button>
+  </form>
+</div>
+  |============================
+*/
